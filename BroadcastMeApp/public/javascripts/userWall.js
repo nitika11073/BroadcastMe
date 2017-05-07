29fb9fd92bb8830ee1e845dc22905246be354e81
nitika11073 <nitika11073@iiitd.ac.in>
2017-05-07 17:49:05 +0530

    layout for user home page

--- BroadcastMeApp/public/javascripts/userWall.js
+++ BroadcastMeApp/public/javascripts/userWall.js
@@ -4,7 +4,8 @@ BroadcastMe.userWall = (function(){
 	var cache, data, fn, api;
 	
 	cache = {
-			
+			wallPosts : $(".wall-posts"),
+			followUsers : $(".follow-users")
 	};
 	
 	fn = {
@@ -12,8 +13,28 @@ BroadcastMe.userWall = (function(){
 				fn._bindEvents();
 			},
 			_bindEvents : function() {
-				
+				fn._getWallPosts();
+				fn._getFollowingUsers();
+			},
+			_getWallPosts : function(){
+				$.ajax({
+					url : 'getFollowingPosts'
+				}).done(function(response) {
+					response.forEach(function(post, index) {
+						cache.wallPosts.append("<p>"+post.content+"</p>");
+					});
+				});
+			},
+			_getFollowingUsers : function(){
+				$.ajax({
+					url : 'getFollowingUsers'
+				}).done(function(response) {
+					response.forEach(function(user, index) {
+						cache.followUsers.append("<p>"+user.name+"</p>");
+					});
+				});
 			}
+	}
 	
 	api = {
 			init: function(){ 
